53020f65931cebf4aa7fad98927a67e9f4e0b3b4
Sarthak <ss22072000@gmail.com>
2024-07-25 13:23:44 +0530

    Remeoving favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="icon" href="/favicon.ico" />
       <body className={font.className}>
         <Suspense fallback={<FullPageLoader />}>{children}</Suspense>
       </body>
